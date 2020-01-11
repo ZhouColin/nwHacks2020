9d1f6d08e6be4d563941c991313d0038081ac80b
Tawshif Alam <tawshif27@gmail.com>
2020-01-11 15:58:16 -0800

    Add global module resolvers

--- front_end/babel.config.js
+++ front_end/babel.config.js
@@ -1,3 +1,30 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: [
+          '.js',
+          '.jsx',
+          '.es',
+          '.es6',
+          '.mjs',
+          '.tsx',
+          '.ts',
+          '.ios.js',
+          '.android.js',
+          '.json',
+        ],
+        root: ['./'],
+        alias: {
+          components: './app/components',
+          features: './app/features',
+          store: './app//store',
+          styles: './app/styles',
+          routes: './app/routes',
+        },
+      },
+    ],
+  ],
 };

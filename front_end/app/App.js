7481af5b432fac0bf13527a65b48b84bb56fa0b8
Tawshif Alam <tawshif27@gmail.com>
2020-01-11 18:04:47 -0800

    connect ot redux store

--- front_end/app/App.js
+++ front_end/app/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import 'react-native-gesture-handler';
 import AppContainer from 'routes/index';
+import store from 'store/index';
+import {Provider} from 'react-redux';
 
-const App = () => <AppContainer />;
+const App = () => (
+  <Provider store={store}>
+    <AppContainer />
+  </Provider>
+);
 
 export default App;
